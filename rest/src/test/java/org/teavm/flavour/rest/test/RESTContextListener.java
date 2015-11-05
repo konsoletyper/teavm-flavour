1a284e8a93a19a4cf66804ed08ad63f58972c09b
Alexey Andreev <konsoletyper@gmail.com>
2015-11-05 23:05:40 +0300

    Integration tests for REST client running

---
+++ rest/src/test/java/org/teavm/flavour/rest/test/RESTContextListener.java
@@ -0,0 +1,50 @@
+/*
+ *  Copyright 2015 Alexey Andreev.
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+package org.teavm.flavour.rest.test;
+
+import java.util.EnumSet;
+import javax.servlet.DispatcherType;
+import javax.servlet.FilterRegistration;
+import javax.servlet.ServletContext;
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
+import javax.servlet.ServletRegistration.Dynamic;
+import javax.servlet.annotation.WebListener;
+import org.eclipse.jetty.servlets.CrossOriginFilter;
+import org.glassfish.jersey.servlet.ServletContainer;
+
+/**
+ *
+ * @author Alexey Andreev
+ */
+@WebListener
+public class RESTContextListener implements ServletContextListener {
+    @Override
+    public void contextInitialized(ServletContextEvent sce) {
+        ServletContext ctx = sce.getServletContext();
+        Dynamic servlet = ctx.addServlet("jersey", ServletContainer.class);
+        servlet.addMapping("/*");
+        servlet.setInitParameter("jersey.config.server.provider.classnames", TestServiceImpl.class.getName());
+
+        FilterRegistration.Dynamic filter = ctx.addFilter("origin", CrossOriginFilter.class);
+        filter.setInitParameter("allowedMethods", "GET,POST,PUT,DELETE");
+        filter.addMappingForUrlPatterns(EnumSet.allOf(DispatcherType.class), true, "/*");
+    }
+
+    @Override
+    public void contextDestroyed(ServletContextEvent sce) {
+    }
+}
